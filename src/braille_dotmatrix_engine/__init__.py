0ae1db6109dfd92b86e247372210df9c4a21fdcc
selinyi123 <100994333+selinyi123@users.noreply.github.com>
2026-06-19 10:38:36 +0800

    Update package exports

--- src/braille_dotmatrix_engine/__init__.py
+++ src/braille_dotmatrix_engine/__init__.py
@@ -1,1 +1,5 @@
-from .engine import BrailleArtConfig, create_demo_image, process_image
+from .config import BrailleArtConfig, MaterialProfile, PrinterProfile, TactileGeometry
+from .pipeline import create_demo_image, process_image
+from .braille_unicode import braille_matrix_to_text, decode_braille_cell, encode_braille_cell, encode_to_braille_matrix, unicode_roundtrip_test
+
+__all__ = ['BrailleArtConfig', 'MaterialProfile', 'PrinterProfile', 'TactileGeometry', 'create_demo_image', 'process_image', 'encode_braille_cell', 'decode_braille_cell', 'encode_to_braille_matrix', 'braille_matrix_to_text', 'unicode_roundtrip_test']
