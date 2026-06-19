32f79a77e71e2ea0f98383ae32c48e27d090a95f
selinyi123 <100994333+selinyi123@users.noreply.github.com>
2026-06-20 00:48:33 +0800

    export quality control APIs

--- src/braille_dotmatrix_engine/__init__.py
+++ src/braille_dotmatrix_engine/__init__.py
@@ -1,8 +1,9 @@
 from .config import BrailleArtConfig, MaterialProfile, PrinterProfile, TactileGeometry
 from .pipeline import create_demo_image, process_image
 from .chromatic import build_chromatic_array, render_chromatic_png
-from .ascii_backend import render_ascii_text, write_ascii_output
+from .ascii_backend import ASCII_PRESETS, render_ascii_html, render_ascii_text, resolve_ascii_charset, write_ascii_output
 from .braille_enhance import enhance_sampled_values
+from .braille_quality import analyze_braille_quality, apply_density_control
 from .braille_unicode import (
     BRAILLE_BASE,
     BRAILLE_BIT_LAYOUT,
@@ -25,9 +26,14 @@ __all__ = [
     'process_image',
     'build_chromatic_array',
     'render_chromatic_png',
+    'ASCII_PRESETS',
+    'resolve_ascii_charset',
     'render_ascii_text',
+    'render_ascii_html',
     'write_ascii_output',
     'enhance_sampled_values',
+    'apply_density_control',
+    'analyze_braille_quality',
     'encode_braille_cell',
     'decode_braille_cell',
     'encode_to_braille_matrix',
