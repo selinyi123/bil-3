ff96b9a5d6e8b9111c0c813b3936cee99266dff1
selinyi123 <100994333+selinyi123@users.noreply.github.com>
2026-06-19 14:33:28 +0800

    export braille matrix decoder

--- src/braille_dotmatrix_engine/__init__.py
+++ src/braille_dotmatrix_engine/__init__.py
@@ -1,5 +1,29 @@
 from .config import BrailleArtConfig, MaterialProfile, PrinterProfile, TactileGeometry
 from .pipeline import create_demo_image, process_image
-from .braille_unicode import braille_matrix_to_text, decode_braille_cell, encode_braille_cell, encode_to_braille_matrix, unicode_roundtrip_test
+from .braille_unicode import (
+    BRAILLE_BASE,
+    BRAILLE_BIT_LAYOUT,
+    braille_matrix_to_text,
+    decode_braille_cell,
+    decode_braille_matrix,
+    encode_braille_cell,
+    encode_to_braille_matrix,
+    unicode_roundtrip_test,
+)
 
-__all__ = ['BrailleArtConfig', 'MaterialProfile', 'PrinterProfile', 'TactileGeometry', 'create_demo_image', 'process_image', 'encode_braille_cell', 'decode_braille_cell', 'encode_to_braille_matrix', 'braille_matrix_to_text', 'unicode_roundtrip_test']
+__all__ = [
+    'BRAILLE_BASE',
+    'BRAILLE_BIT_LAYOUT',
+    'BrailleArtConfig',
+    'MaterialProfile',
+    'PrinterProfile',
+    'TactileGeometry',
+    'create_demo_image',
+    'process_image',
+    'encode_braille_cell',
+    'decode_braille_cell',
+    'encode_to_braille_matrix',
+    'decode_braille_matrix',
+    'braille_matrix_to_text',
+    'unicode_roundtrip_test',
+]
